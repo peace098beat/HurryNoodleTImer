f4a6bdb48f86718c17186144ad076b44bf726508
野原 友幸 <peace098beat.t@gmail.com>
2015-06-05 00:08:30 +0900

    タイマー関数を作成

--- js/main.js
+++ js/main.js
@@ -2,18 +2,35 @@ console.log("== Run main.js ==");
 
 myCnt = 0;
 myTim = 0;
+flgTim = 0; //0:停止, 1:アクティブ
 
 function myGo(){
-myCnt = 59;
-myTim = setInterval ( "myTimer()", 1000 );
+	if( flgTim ){
+		// 1:アクティブ中
+		// タイマー停止
+		flgTim = 0;
+		reset();
+
+	}else{
+		// 0:停止中
+		flgTim = 1;
+		myCnt = 60;
+		myTim = setInterval ( "myTimer()", 1000 );
+	}
 }
 
 function myTimer(){
-myCnt = myCnt - 1;
-document.getElementById( "countdown" ).innerHTML = "02:" + myCnt + "";
-document.getElementsByClassName("countdown").innerHTML = "02:" + myCnt + "";
-if ( myCnt == 0 ){
-clearInterval( myTim );
-alert( "ラーメン食べ頃だよ！！" );
+	myCnt = myCnt - 1;
+	document.getElementById( "time" ).innerHTML = "02:" + myCnt + "";
+	if ( myCnt == 10 ){
+		clearInterval( myTim );
+		alert( "ラーメン食べ頃だよ！！" );
+	}
+	console.log('== run myTimer ==');
+	console.log('myCnt'+myCnt);
 }
+
+function reset(){
+	clearInterval( myTim );
+	document.getElementById( "time" ).innerHTML = "03:00";
 }
